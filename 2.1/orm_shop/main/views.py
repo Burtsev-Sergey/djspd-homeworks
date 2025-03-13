c071aa1faf23f218771c3ff6a777bf1d328dd906
Burtsev Sergey <sburtsevs@mail.ru>
2025-03-13 17:40:37 +0300

    There are 3 page controllers implemented in views

--- 2.1/orm_shop/main/views.py
+++ 2.1/orm_shop/main/views.py
@@ -5,21 +5,26 @@ from main.models import Car
 
 
 def cars_list_view(request):
-    # получите список авто
+    cars = Car.objects.all()
     template_name = 'main/list.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    return render(request, template_name, context={"cars": cars})
 
 
 def car_details_view(request, car_id):
-    # получите авто, если же его нет, выбросьте ошибку 404
-    template_name = 'main/details.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+	try:
+		car = Car.objects.get(pk=car_id)
+		context = {'car': car}
+		template_name = 'main/details.html'
+		return render(request, template_name, context)
+	except Car.DoesNotExist:
+		raise Http404('Car not found')
 
 
 def sales_by_car(request, car_id):
     try:
-        # получите авто и его продажи
+        car = Car.objects.get(pk=car_id)
+        sales = car.sale_set.all()
         template_name = 'main/sales.html'
-        return render(request, template_name, {})  # передайте необходимый контекст
+        return render(request, template_name, context={'car': car, 'sales': sales})  # передайте необходимый контекст
     except Car.DoesNotExist:
         raise Http404('Car not found')
